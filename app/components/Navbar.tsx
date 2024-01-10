976b1b6cf273bb530277e851854d293c93fcb4a7
Abdulrahman Sadiq <61137642+master-keyCW@users.noreply.github.com>
2024-01-10 20:09:34 +0100

    git commit -m

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,42 @@
+// import Logo from "../components/logo"
+import {cn} from "../lib/utils"
+import Link  from 'next/link'
+
+
+interface Props{
+  className?:string;
+  spanClassName?:string;
+}
+
+const Navbar = ({className,spanClassName}: Props) => {
+
+  const navigation = [
+    {_id:910, title:"Home", href:"/"},
+    {_id:911, title:"Phone", href:"/phones"},
+    {_id:912, title:"Phone Cases", href:"/phonecases"},
+    {_id:913, title:"Watches", href:"/watches"},
+    {_id:914, title:"Accessorries", href:"/accessories"},
+  ]
+
+  return (
+    <div className="w-full h-20 border-b-[1px] border-b-zinc-500 bg-white text-zinc-600">
+        <div className="max-w-screen-xl mx-auto h-full flex items-center">
+          <Link href={"/"} className={cn('text-zinc-950 text-xl underline underline-offset-4 decoration-[1px] group')}>
+            <span className={cn('bg-zinc-950 text-white w-8 h-8 rounded-full inline-flex items-center justify-center text-2xl font-bold mr-1 group-hover:bg-blue-700 duration-200', spanClassName)}>N</span>iceshop
+          </Link>
+
+          <ul>
+            {navigation.map((item) => (
+                // eslint-disable-next-line react/jsx-key
+                <Link href={item?.href}>
+                  <li>{item?.title}</li>
+                </Link>
+              ))}
+          </ul>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ app/components/logo.tsx
@@ -0,0 +1,9 @@
+import Link  from 'next/link'
+import {cn} from "../lib/utils"
+
+
+export const logo = ({className,spanClassName}: any) => {
+  return (
+    
+  )
+}
